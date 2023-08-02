87ea182b37d85b773ed22666f27e7b45acc9ea93
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-08-01 19:59:57 -0500

    update

--- lib/src/enum/api.dart
+++ lib/src/enum/api.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
 class API {
-  static const String API_URL = "https://utmfamilyhealth.site";
-  static const String API_URL_OLD = "utmfamilyhealth.site";
+  // static const String API_URL = "https://utmfamilyhealth.site";
+  // static const String API_URL_OLD = "utmfamilyhealth.site";
+
+  static const String API_URL = "http://192.168.100.106:3000";
+  static const String API_URL_OLD = "192.168.100.106:3000";
 }
