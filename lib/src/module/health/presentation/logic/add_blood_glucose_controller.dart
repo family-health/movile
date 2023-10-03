190ab00a30c5cbaa357ffb669ee2d8a064a1f719
Joseph Garcia <josephdgb1996@gmail.com>
2023-10-03 00:41:13 -0500

    add glucose changes

--- lib/src/module/health/presentation/logic/add_blood_glucose_controller.dart
+++ lib/src/module/health/presentation/logic/add_blood_glucose_controller.dart
@@ -1,11 +1,15 @@
+import 'package:app/src/@core/utilities/snackbar.dart';
 import 'package:app/src/module/health/domain/usecases/add_blood_glucose_usecase.dart';
+import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
 import 'package:health/health.dart';
+
+import '../../../../@core/error/failures.dart';
 
 class AddBloodGlucoseController extends GetxController {
   late double value;
 
-  void onChange(String? value){
+  void onChange(String? value) {
     this.value = double.parse(value!);
     // ignore: avoid_print
     print(value);
@@ -14,7 +18,13 @@ class AddBloodGlucoseController extends GetxController {
   void saveBloodGlucose() async {
     final now = DateTime.now();
     AddBloodGlucoseUsecase addBloodGlucoseUsecase = Get.find<AddBloodGlucoseUsecase>();
-    addBloodGlucoseUsecase.call(AddBloodGlucoseParams(value, HealthDataType.BLOOD_GLUCOSE, now, HealthDataUnit.MILLIGRAM_PER_DECILITER));
+    Either<Failure, bool> result = await addBloodGlucoseUsecase.call(AddBloodGlucoseParams(value, HealthDataType.BLOOD_GLUCOSE, now, HealthDataUnit.MILLIGRAM_PER_DECILITER));
+    result.fold((l) {
+      Get.back();
+      Snackbar().error("Error", "Error when add glucose");
+    }, (r) {
+      Get.back();
+      Snackbar().success("Glucose added", "Glucose added succesfully");
+    });
   }
-
 }
